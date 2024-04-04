a4e65446e0d0f43baeb416c8ce3865443e6ea844
HBnkgv5JCyL2l59WG7ieKCxi0jsij0few8b0mYfPxX4= <rathimalathi99@gmail.com>
2024-04-04 16:59:01 +0530

    Web automation framework setup

---
+++ pages/Before_signIn_page.py
@@ -0,0 +1,12 @@
+import time
+from selenium.webdriver.common.by import By
+from time import sleep
+from configs.properties import Properties
+# from commons import lib_global
+
+class Before_signIn:
+    """This class consists of automated tests of Login Page"""
+
+    def launchURL(driver):
+        driver.get(Properties.url)
+        time.sleep(5)
---
+++ steps/before_signIn_step.py
@@ -0,0 +1,6 @@
+# from selenium import
+from behave import given, when, then
+from pages.Before_signIn_page import Before_signIn
+
+login_obj = Before_signIn
+driver_variables = []
